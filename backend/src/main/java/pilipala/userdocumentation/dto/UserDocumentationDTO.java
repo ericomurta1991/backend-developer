9ebacda5e96165818eabed1fccaae6cdac8332e4
ericomurta1991 <erico_murta@yahoo.com.br>
2025-08-11 03:33:28 -0300

    adicionado anotações swagger para exemplos nas requisições

--- backend/src/main/java/pilipala/userdocumentation/dto/UserDocumentationDTO.java
+++ backend/src/main/java/pilipala/userdocumentation/dto/UserDocumentationDTO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,15 +18,20 @@ import pilipala.userdocumentation.enums.DocumentType;
 public class UserDocumentationDTO implements Serializable{
 	private static final long serialVersionUID =1L;
 	
+	@Schema(description = "ID do documento", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
 	private Long id;
+	
 	@NotNull(message = "Tipo de Documento é obrigatório")
+	@Schema(description = "Tipo do documento", example = "CPF", allowableValues = {"CPF", "CNPJ", "Outros"})
     private DocumentType type;
 	
 	@NotBlank(message = "Numero do documento  é obrigatório")
+	@Schema(description = "Número do documento", example = "123.456.789-00")
     private String number;
 	
 	@JsonIgnore
 	@NotNull(message = "Documento é obrigatório")
+	@Schema(description = "Arquivo do documento em base64", accessMode = Schema.AccessMode.READ_ONLY)
 	private byte[] document;
     private Long userId;
 	    
